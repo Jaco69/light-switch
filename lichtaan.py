5835c10ea99c2bc7362cb21639bc21f20368ae64
Jaco van Iterson <jaco.van.iterson@gmail.com>
2014-12-10 10:01:25 +0100

    Fixed crash
    Previous version crashed as the next dusk/dawn calculations didn't give
    the expected result. Setting huis.date to today fixed this

--- lichtaan.py
+++ lichtaan.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 #
 # schakelt automatisch het licht aan als het nacht wordt
 #
@@ -8,6 +9,12 @@ import pifacedigitalio as p
 slaap = 0   # slapen om 0 uur na middernacht
 wakker = 7  # wakker om 7 uur na middernacht
 wacht = 4   # wacht 4 uur na slaap om DST te berekenen
+
+def sleep(tijd):
+  print ("begin slaap voor", tijd / ephem.second, "seconden om", ephem.localtime(ephem.now()))
+  time.sleep(tijd / ephem.second)
+  print ("stop slaap om", ephem.localtime(ephem.now()))
+
 p.init()
 huis = ephem.Observer()
 huis.lat = "52.1856"
@@ -15,41 +22,41 @@ huis.lon = "4.44541"
 huis.elev = 1
 huis.horizon = '-2.0' # -6=civil twilight, -12=nautical, -18=astronomical
 zon = ephem.Sun()
-zon.compute()
 ochtend = huis.next_rising(zon, use_center=True)
 avond = huis.next_setting(zon, use_center=True)
 if ((avond - ephem.now()) < (ochtend-ephem.now())):
   # het is nu dag wacht tot de avond
-  time.sleep((avond - ephem.now()) * 24 * 3600)
+  sleep(avond - ephem.now())
 else:
   # het is al donker
   avond = ephem.now()
 
 while (True):
   if (time.localtime().tm_isdst == 1):
-    slaapuit =  ephem.Date(slaap * ephem.hour + 0.5 + int(ochtend) + time.altzone * ephem.second)
+    slaapuit =  ephem.Date(slaap * ephem.hour + 0.5 + int(ochtend) + time.altzone  * ephem.second)
   else:
     slaapuit =  ephem.Date(slaap * ephem.hour + 0.5 + int(ochtend) + time.timezone * ephem.second)
-  # het is avond, doe het licht aan
-  p.digital_write(1, 1) #licht aan
-  time.sleep((slaapuit - avond) * 24 * 3600)
+  if (slaapuit > avond):
+    # het is avond, doe het licht aan
+    p.digital_write(1, 1) #licht aan
+  sleep(slaapuit - ephem.now())
   # we zijn naar bed, doe het licht uit
   p.digital_write(0, 0) #licht uit
-  time.sleep(wacht * 3600) # wacht x uur voor eventuele zomer/winter tijd verandering
+  sleep(wacht * ephem.hour) # wacht x uur voor eventuele zomer/winter tijd verandering
   if (time.localtime().tm_isdst == 1):
     wakkeraan = ephem.Date(wakker * ephem.hour + 0.5 + int(ochtend) + time.altzone * ephem.second)
   else:
     wakkeraan = ephem.Date(wakker * ephem.hour + 0.5 + int(ochtend) + time.timezone * ephem.second)
-  time.sleep(((wakkeraan - slaapuit) * 24 - wacht)* 3600) # wacht de rest van de tijd tot we wakker zijn
+  sleep(wakkeraan - slaapuit - wacht * ephem.hour) # wacht de rest van de tijd tot we wakker zijn
   # we zijn wakker, is het nog donker doe dan het licht aan
   if (ochtend > wakkeraan):
     p.digital_write(0, 1) #licht aan
-    time.sleep((ochtend - wakkeraan) * 24 * 3600)
+    sleep(ochtend - wakkeraan)
     p.digital_write(0, 0) #licht uit
-    print("Ochtend licht uit", ephem.localtime(ephem.now()))
   # bereken tijden voor nieuwe nacht
-  time.sleep(3600)
-  zon.compute()
+  huis.date = ochtend + 1 * ephem.hour
+  print ("nu", ephem.localtime(ephem.now()), " avond", ephem.localtime(avond), " ochtend", ephem.localtime(ochtend))
   avond = huis.next_setting(zon, use_center=True)
   ochtend = huis.next_rising(zon, use_center=True)
-  time.sleep((avond - ephem.now()) * 24 * 3600)
+  print ("Volgende                       avond", ephem.localtime(avond), " ochtend", ephem.localtime(ochtend))
+  sleep(avond - ephem.now())
